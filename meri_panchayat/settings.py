6e2329679187b8eef5fec3d72d8a7845230832e1
Adarsh Pal <adarshpal8853@gmail.com>
2026-09-14 15:12:03 +0530

    Configure PostgreSQL database

--- meri_panchayat/settings.py
+++ meri_panchayat/settings.py
@@ -76,10 +76,19 @@ WSGI_APPLICATION = 'meri_panchayat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DATABASE"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": "5432",
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
